0fc5d1b0fdae3c5eff9e6822748b40917941aa15
Aleksei Evstratov <alekseie@dmdevelopment.ru>
2021-10-06 16:35:04 +0700

    Clean branch

--- lab_1_24/Program.cs
+++ lab_1_24/Program.cs
@@ -1,12 +1,1 @@
-﻿using System;
-
-namespace lab_1_24
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+﻿
